5ac8b072c21ce6115ec284d441233dbf83deae26
Hung <nth3122000@gmail.com>
2023-02-20 20:48:09 +0700

    done config jasmine

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -7,7 +7,7 @@ import SuiteInfo = jasmine.SuiteInfo;
 
 class CustomProcessor extends DisplayProcessor {
   public displayJasmineStarted(info: SuiteInfo, log: string): string {
-    return `${log} ${info}`;
+    return `${log}`;
   }
 }
 
